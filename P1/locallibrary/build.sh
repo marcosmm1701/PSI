1d2236ed044b3538876cd1c35a5affa5bd20e346
marcosmm1701 <marcosmunozmerchan@gmail.com>
2025-02-17 22:59:05 +0100

    collectstatic --noinput en build

--- P1/locallibrary/build.sh
+++ P1/locallibrary/build.sh
@@ -1,7 +1,6 @@
 pip3 install -r requirements.txt
+python manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
-python manage.py collectstatic
 python3 manage.py runscript populate_catalog
 python3 manage.py createsuperuser
-python3 manage.py runserver
