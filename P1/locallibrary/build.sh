5c12f1d186b3db0a4351df586934d620215c1846
marcosmm1701 <marcosmunozmerchan@gmail.com>
2025-02-17 22:39:33 +0100

    collectstatic en el build

--- P1/locallibrary/build.sh
+++ P1/locallibrary/build.sh
@@ -1,6 +1,7 @@
 pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
+python manage.py collectstatic
 python3 manage.py runscript populate_catalog
 python3 manage.py createsuperuser
 
