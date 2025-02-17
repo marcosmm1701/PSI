08982992f9ccb3d274900af148dfacd986c33380
marcosmm1701 <marcosmunozmerchan@gmail.com>
2025-02-18 00:30:25 +0100

    s

--- P1/locallibrary/build.sh
+++ P1/locallibrary/build.sh
@@ -3,8 +3,9 @@ pip3 install -r requirements.txt
 
 python3 manage.py makemigrations
 python3 manage.py migrate
+
 python3 manage.py populate_catalog
+python manage.py collectstatic --noinput
 
 python3 manage.py createsuperuser
 
-python manage.py collectstatic --noinput
