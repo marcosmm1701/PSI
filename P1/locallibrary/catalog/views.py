89dccb42ab62721e99a9a43e0ebdb3791db7a11c
marcosmm1701 <marcosmunozmerchan@gmail.com>
2025-02-17 19:04:56 +0100

    Todos los test y coverage Terminado

--- P1/locallibrary/catalog/views.py
+++ P1/locallibrary/catalog/views.py
@@ -173,15 +173,6 @@ class AuthorDelete(PermissionRequiredMixin, DeleteView):
     success_url = reverse_lazy('authors')   # Redirige a la lista de autores si todo va bien
     permission_required = 'catalog.delete_author'
 
-    def form_valid(self, form):
-        try:
-            self.object.delete()
-            return HttpResponseRedirect(self.success_url)
-        except Exception as e:
-            return HttpResponseRedirect(
-                reverse("author-delete", kwargs={"pk": self.object.pk})
-            )
-
 
 
 
@@ -191,6 +182,7 @@ class BookCreate(PermissionRequiredMixin, CreateView):
     model = Book
     fields = ['title', 'author', 'summary', 'isbn', 'genre', 'language']
     permission_required = 'catalog.add_book'
+    permission_required = 'catalog.can_mark_returned'
     success_url = reverse_lazy('books')
 
 class BookUpdate(PermissionRequiredMixin, UpdateView):
@@ -203,15 +195,6 @@ class BookDelete(PermissionRequiredMixin, DeleteView):
     model = Book
     success_url = reverse_lazy('books')   # Redirige a la lista de autores si todo va bien
     permission_required = 'catalog.delete_book'
-
-    def form_valid(self, form):
-        try:
-            self.object.delete()
-            return HttpResponseRedirect(self.success_url)
-        except Exception as e:
-            return HttpResponseRedirect(
-                reverse("book-delete", kwargs={"pk": self.object.pk})
-            )
 
 
 
