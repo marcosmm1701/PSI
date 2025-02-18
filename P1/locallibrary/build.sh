adbd0ef7c8cd74b24e68504a844fcbca1e079d84
marcosmm1701 <marcosmunozmerchan@gmail.com>
2025-02-18 14:32:40 +0100

    Super usuario en build

--- P1/locallibrary/build.sh
+++ P1/locallibrary/build.sh
@@ -8,5 +8,13 @@ python3 populate_catalog.py
 
 python manage.py collectstatic --noinput
 
-python3 manage.py createsuperuser
+python3 manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "tucontraseña123")
+    print("Superusuario creado exitosamente.")
+else:
+    print("El superusuario ya existe.")
+EOF
 
