12fb804b05cfd9cfe81f2a61909824b52521cf9e
marcosmm1701 <marcosmunozmerchan@gmail.com>
2025-02-18 19:49:30 +0100

    FInal sin FLAKE

--- P1/locallibrary/locallibrary/settings.py
+++ P1/locallibrary/locallibrary/settings.py
@@ -26,15 +26,13 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-38kdef3zlgo)t9uy!c(=#-^u06o%fxh53!-rcrxe#^qk!y+*v+'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
                 #psi-ruhh.onrender.com,localhost,127.0.0.1
-ALLOWED_HOSTS = ['*']
-
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
@@ -99,6 +97,7 @@ if os.getenv("TESTING") == 'True':
         'HOST': 'localhost',
         'PORT': '5432',
     }
+    
 else:
     print("USANDO BASE DE DATOS NEON")
     db_from_env = dj_database_url.config(
