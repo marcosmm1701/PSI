8bb300641cf49844f9f783ffbfb779260a158f7e
IgnacioSerena <ignacio.sermon@gmail.com>
2025-03-07 09:25:59 +0100

    1 hecho

--- P1/locallibrary/catalog/forms.py
+++ P1/locallibrary/catalog/forms.py
@@ -12,7 +12,7 @@ class RenewBookForm(forms.Form):
         # Check if a date is not in the past.
         if data < datetime.date.today():
             raise ValidationError(_('Invalid date - renewal in past'))
-
+ 
         # Check if a date is in the allowed range (+4 weeks from today).
         if data > datetime.date.today() + datetime.timedelta(weeks=4):
             raise ValidationError(_('Invalid date - renewal more than 4 weeks ahead'))
--- P1/locallibrary/catalog/models.py
+++ P1/locallibrary/catalog/models.py
@@ -14,7 +14,7 @@ class Genre(models.Model):
     """Model representing a book genre."""
     name = models.CharField(
         max_length=200,
-        unique=True,
+        unique=True, 
         help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)"
     )
 
