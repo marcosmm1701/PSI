432159499b259387e02fee06097088476a48a774
marcosmm1701 <marcosmunozmerchan@gmail.com>
2025-03-10 18:47:53 +0100

    Parte 3 termianda

---
+++ P2/twopages/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+<template>
+  <div>
+    <h1>Ejemplo de uso de Router</h1>
+    <nav>
+      <router-link to="/page-one">Click for Page 1</router-link>&nbsp;
+      <router-link to="/page-two">Click for Page 2</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {}
+</script>
---
+++ P2/twopages/src/components/PageOne.vue
@@ -0,0 +1,8 @@
+<template>
+
+    <h1>Hi! I am page ONE! </h1>
+</template>
+
+<script>
+export default {}
+</script>
