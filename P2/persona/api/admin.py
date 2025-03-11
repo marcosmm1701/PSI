73d0fbeaa454e1d48a6f59f9c620053c42b5263d
marcosmm1701 <marcosmunozmerchan@gmail.com>
2025-03-11 12:46:22 +0100

    admin, allowed hosts, settings, .env

--- P2/persona/api/admin.py
+++ P2/persona/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Persona
 
 # Register your models here.
+class PersonaAdmin(admin.ModelAdmin):
+    model=Persona
+    list_display = ["id","nombre","apellido", "email"]
+
+admin.site.register(Persona, PersonaAdmin)
