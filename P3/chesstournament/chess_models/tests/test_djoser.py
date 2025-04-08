f69421c6d2a9d66799b1c9b2910da2c1176c4cca
marcosmm1701 <marcosmunozmerchan@gmail.com>
2025-04-08 22:10:16 +0200

    quitamos /auth para homogeneizar las consultas log in y users

--- P3/chesstournament/chess_models/tests/test_djoser.py
+++ P3/chesstournament/chess_models/tests/test_djoser.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from django.contrib.auth.models import User
 
 # You may modoify this variables to match your project
-BASE_URL = 'http://127.0.0.1:8081/api/v1/auth/'
+BASE_URL = 'http://127.0.0.1:8081/api/v1/'
 
 # Do not modify the code below
 
