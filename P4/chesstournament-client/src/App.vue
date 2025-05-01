533428fba0880a4f9a297aed1120719dcb271798
marcosmm1701 <marcosmunozmerchan@gmail.com>
2025-05-01 22:37:25 +0200

    Iniciamos aventura Practica 4

---
+++ P4/chesstournament-client/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+<template>
+<div>
+<h1>Ejemplo de uso de Router</h1>
+<router-view></router-view>
+</div>
+</template>
+<script>
+export default {}
+</script>
---
+++ P4/chesstournament-client/src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+// Importamos el CSS de Bootstrap
+import './assets/main.css'
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
+
+// Importa el archivo JavaScript de Bootstrap desde node_modules
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+
+// Importa el archivo CSS de Bootstrap desde node_modules
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
